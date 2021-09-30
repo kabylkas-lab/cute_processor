36e5af2120e441303a433ae9652f364abb331fff
ZDaniyar <87701841+ZDaniyar@users.noreply.github.com>
2021-09-30 17:17:47 +0600

    Add files via upload

---
+++ rtl/mux.v
@@ -0,0 +1,6 @@
+module m81(output out, input D0, D1, D2, D3, D4, D5, D6, D7, S0, S1, S2);
+assign S1bar=~S1;
+assign S0bar=~S0;
+assign S2bar=~S2;
+assign out = (D0 & S2bar & S1bar & S0bar) | (D1 & S2bar & S1bar & S0) | (D2 & S2bar & S1 & S0bar) + (D3 & S2bar & S1 & S0) + (D4 & S2 & S1bar & S0bar) + (D5 & S2 & S1bar & S0) + (D6 & S2 & S1 & S0bar) + (D7 & S2 & S1 & S0);
+endmodule
