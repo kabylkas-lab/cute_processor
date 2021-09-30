7cf6278e033bd14a30949957fd28b28b3c4c9881
ZDaniyar <87701841+ZDaniyar@users.noreply.github.com>
2021-09-30 21:25:00 +0600

    Add files via upload

--- rtl/mux.v
+++ rtl/mux.v
@@ -1,6 +1,7 @@
-module m81(output out, input D0, D1, D2, D3, D4, D5, D6, D7, S0, S1, S2);
-assign S1bar=~S1;
-assign S0bar=~S0;
-assign S2bar=~S2;
-assign out = (D0 & S2bar & S1bar & S0bar) | (D1 & S2bar & S1bar & S0) | (D2 & S2bar & S1 & S0bar) + (D3 & S2bar & S1 & S0) + (D4 & S2 & S1bar & S0bar) + (D5 & S2 & S1bar & S0) + (D6 & S2 & S1 & S0bar) + (D7 & S2 & S1 & S0);
+module m81(output out, input a, b, c, d, e, f, g, h, i, j, c1, c2, c3, c4);
+assign c1bar=~c1;
+assign c2bar=~c2;
+assign c3bar=~c3;
+assign c4bar=~c4;
+assign otp = (a & c1bar & c2bar & c3bar & c4bar) | (b & c1bar & c2bar & c3bar & c4) | (c & c1bar & c2bar & c3 & c4bar) + (d & c1bar & c2bar & c3 & c4) + (e & c1bar & c2 & c3bar & c4bar) + (f & c1bar & c2 & c3bar & c4) + (g & c1bar & c2 & c3 & c4bar) + (h & c1bar & c2 & c3 & c4) + (i & c1 & c2bar & c3bar & c4bar) + (j & c1 & c2bar & c3bar & c4);
 endmodule
