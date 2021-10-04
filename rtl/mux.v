6b06d9e44774175bf6fa7e628d9f4634a2b091d3
Terrofont <33019134+Terrofont@users.noreply.github.com>
2021-10-04 18:07:02 +0600

    employ 4 bit fields

--- rtl/mux.v
+++ rtl/mux.v
@@ -1,12 +1,10 @@
-module mux(otp, a, b, c, d, e, f, g, h, i, j, c1, c2, c3, c4);
-input wire a, b, c, d, e, f, g, h, i, j, c1, c2, c3, c4;
+module mux(otp, a, b, c, d, e, f, g, h, i, j, ctrlVar);
+input wire a, b, c, d, e, f, g, h, i, j, ctrlVar;
 output reg otp;
 
-wire [3:0] select;
-assign select = {c1, c2, c3, c4};
 always@(*)
     begin
-    case(select)
+    case(ctrlVar)
     4'b0000: otp=a;
     4'b0001: otp=b;
     4'b0010: otp=c;
@@ -15,8 +13,8 @@ always@(*)
     4'b0101: otp=f;
     4'b0110: otp=g;
     4'b0111: otp=h;
-    4'b1101: otp=i;
-    4'b1110: otp=j;
+    4'b1000: otp=i;
+    4'b1001: otp=j;
     default: otp=1'b0;
     endcase
 end
