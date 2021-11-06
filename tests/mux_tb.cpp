93e8804b299adb081c82bd800d20b5049b24e808
Terrofont <33019134+Terrofont@users.noreply.github.com>
2021-11-06 18:40:15 +0600

    fix indentation

--- tests/mux_tb.cpp
+++ tests/mux_tb.cpp
@@ -19,24 +19,24 @@ const uint32_t kSimIter = 100;
 uint32_t global_time = 0;
 
 struct input_package {
-	// 9-bit input busses
-	unsigned int a0 : 9;
-	unsigned int a1 : 9;
-	unsigned int a2 : 9;
-	unsigned int a3 : 9;
-	unsigned int a4 : 9;
-	unsigned int a5 : 9;
-	unsigned int a6 : 9;
-	unsigned int a7 : 9;
-	unsigned int a8 : 9;
-	unsigned int a9 : 9;
-	
-	// 4 control switches
-	unsigned int ctrlVar : 4;
+    // 9-bit input busses
+    unsigned int a0 : 9;
+    unsigned int a1 : 9;
+    unsigned int a2 : 9;
+    unsigned int a3 : 9;
+    unsigned int a4 : 9;
+    unsigned int a5 : 9;
+    unsigned int a6 : 9;
+    unsigned int a7 : 9;
+    unsigned int a8 : 9;
+    unsigned int a9 : 9;
+    
+    // 4 control switches
+    unsigned int ctrlVar : 4;
 };
 
 struct output_package {
-	// 9-bit output bus
+    // 9-bit output bus
     unsigned int otp : 9;
 };
 
@@ -64,54 +64,54 @@ int main() {
     int iter = kSimIter;
     while(iter > 0)
     {
-		// generate input package for 9-bit inputs
-		inp.a0 = rand() % 512;
-		inp.a1 = rand() % 512;
-		inp.a2 = rand() % 512;
-		inp.a3 = rand() % 512;
-		inp.a4 = rand() % 512;
-		inp.a5 = rand() % 512;
-		inp.a6 = rand() % 512;
-		inp.a7 = rand() % 512;
-		inp.a8 = rand() % 512;
-		inp.a9 = rand() % 512;
-		
-		//control switches
-		inp.ctrlVar = rand() % 10;
-		
-		// select input by control switches
-		switch (inp.ctrlVar){
-			case 0:
-				out.otp = inp.a0;
-				break;
-			case 1:
-				out.otp = inp.a1;
-				break;
-			case 2:
-				out.otp = inp.a2;
-				break;
-			case 3:
-				out.otp = inp.a3;
-				break;
-			case 4:
-				out.otp = inp.a4;
-				break;
-			case 5:
-				out.otp = inp.a5;
-				break;
-			case 6:
-				out.otp = inp.a6;
-				break;
-			case 7:
-				out.otp = inp.a7;
-				break;
-			case 8:
-				out.otp = inp.a8;
-				break;
-			case 9:
-				out.otp = inp.a9;
-				break;
-		}
+        // generate input package for 9-bit inputs
+        inp.a0 = rand() % 512;
+        inp.a1 = rand() % 512;
+        inp.a2 = rand() % 512;
+        inp.a3 = rand() % 512;
+        inp.a4 = rand() % 512;
+        inp.a5 = rand() % 512;
+        inp.a6 = rand() % 512;
+        inp.a7 = rand() % 512;
+        inp.a8 = rand() % 512;
+        inp.a9 = rand() % 512;
+        
+        //control switches
+        inp.ctrlVar = rand() % 10;
+        
+        // select input by control switches
+        switch (inp.ctrlVar){
+            case 0:
+                out.otp = inp.a0;
+                break;
+            case 1:
+                out.otp = inp.a1;
+                break;
+            case 2:
+                out.otp = inp.a2;
+                break;
+            case 3:
+                out.otp = inp.a3;
+                break;
+            case 4:
+                out.otp = inp.a4;
+                break;
+            case 5:
+                out.otp = inp.a5;
+                break;
+            case 6:
+                out.otp = inp.a6;
+                break;
+            case 7:
+                out.otp = inp.a7;
+                break;
+            case 8:
+                out.otp = inp.a8;
+                break;
+            case 9:
+                out.otp = inp.a9;
+                break;
+        }
 
         //feed data
         top->a0 = inp.a0;
@@ -124,11 +124,11 @@ int main() {
         top->a7 = inp.a7;
         top->a8 = inp.a8;
         top->a9 = inp.a9;
-		top->ctrlVar = inp.ctrlVar;
-		advance_sim(top, trace);
+        top->ctrlVar = inp.ctrlVar;
+        advance_sim(top, trace);
         //top->eval();
 
-		// detect error
+        // detect error
         if(top->otp != out.otp) {
             std::cout << "Error: ctrl = " << inp.ctrlVar << "; ";
             std::cout << "inp.a0 = " << (uint16_t)inp.a0 << "; ";
