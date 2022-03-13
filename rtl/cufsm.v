28846e539914ccf224a18d5677dc4bad367729af
Alexandr Popov <33019134+Terrofont@users.noreply.github.com>
2022-03-13 15:50:43 +0600

    Added mvi instruction

--- rtl/cufsm.v
+++ rtl/cufsm.v
@@ -1,11 +1,13 @@
 // -----------------------------------------------------------------------------
-// FSM
+// CUFSM
 // -----------------------------------------------------------------------------
+/* verilator lint_off UNUSED */
 module cufsm(
     // -------------------------------------------------------------------------
     // Inputs
     // -------------------------------------------------------------------------
     input wire [8:0] ir,
+    input wire Run,
     input wire Resetn,
     input wire clk,
 
@@ -30,11 +32,7 @@ localparam STATE_Initial = 3'd0,
     STATE_alu2 = 3'd2, 
     STATE_alu3 = 3'd3,
     STATE_mv1 = 3'd4,
-    STATE_5_PlaceHolder = 3'd5;
-    //STATE_6_placeHolder = 3'd6,
-    //STATE_7_PlaceHolder = 3'd7;
-    
-
+    STATE_mvi = 3'd5;
 
 reg [2:0] CurrentState;
 reg [2:0] NextState;
@@ -43,10 +41,10 @@ reg [2:0] cmd;
 reg [2:0] adr1;
 reg [2:0] adr2;
 
-assign cmd = ir[6+:3];
-assign adr1 = ir[3+:3];
-assign adr2 = ir[0+:3];
-
+// 000 - ADD
+// 001 - SUB
+// 010 - MV
+// 011 - MVI
 
 // -----------------------------------------------------------------------------
 // Conditional State - Transition
@@ -55,28 +53,37 @@ assign adr2 = ir[0+:3];
 always@ ( posedge clk ) begin
     if ( Resetn || done ) begin
         CurrentState <= STATE_Initial;
-    end else CurrentState <= NextState;
+        IRen <=1;
+    end
+    else begin 
+        CurrentState <= NextState;
+        IRen <= 0;
+    end
 end
 
 
 always@ ( * ) begin
+    cmd = ir[6+:3];
+    adr1 = ir[3+:3];
+    adr2 = ir[0+:3];
     NextState = CurrentState ;
     case ( CurrentState )
         STATE_Initial : begin
             a = 0;
             g = 0;
             rx = 8'd0;
-            mux = 4'd0;
             alu = 0;
             done = 0;
-            if (cmd==0'b010) NextState = STATE_mv1;
-            else NextState = STATE_alu1; 
+            if (adr1 != 3'd0 && adr2 != 3'd0) begin
+                if (cmd==3'b010) NextState = STATE_mv1;
+                else if (cmd[2:1]==2'b00) NextState = STATE_alu1; 
+                else if (cmd==3'b011) NextState = STATE_mvi;
+            end
         end
         STATE_alu1 : begin
             mux = adr1+1;
             rx[adr1] = 1;
             a = 1;
-            
             NextState = STATE_alu2;
             done = 0;
         end
@@ -105,16 +112,11 @@ always@ ( * ) begin
             rx[adr2] = 1;
             done = 1;
         end
-        STATE_5_PlaceHolder: begin
-            if (cmd[2]) NextState = STATE_Initial;
-            else NextState = STATE_Initial;
+        STATE_mvi: begin
+            mux=4'd0;
+            rx[adr1] = 1;
+            done = 1;
         end
-        /*STATE_6_PlaceHolder : begin
-            NextState = STATE_Initial;
-        end
-        STATE_7_PlaceHolder : begin
-            NextState = STATE_Initial;
-        end*/
         default : begin
             NextState = STATE_Initial;
         end
